3dabedf62e2f4157bac92f31dad620cd2139aaf7
Antonio Nascimento <ant.pnasc@gmail.com>
2022-04-26 19:36:10 +0000

    Starting day 2

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,5 +1,16 @@
-from .config import settings
+import typer
 
 
-def main():
-    print("Hello from", settings.NAME)
+main = typer.Typer(help="Beer Management App")
+
+@main.command("add")
+def add(name: str, style: str):
+    """ Add a new beer to database
+    """
+    print(name, style)
+
+@main.command("list")
+def list_beer(style: str):
+    """ List beers from database
+    """
+    print(style)
